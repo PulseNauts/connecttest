a29484b94c87de80c42a113eece86d5920926afa
PulseNauts <pulseNauts@gmail.com>
2024-08-04 17:09:24 -0400

    supply

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
     try {
       const supply = await contractInstance.methods.totalSupply().call();
       console.log('Total supply:', supply);
-      setTotalSupply(Number(supply)); // Convert BigInt to number;
+      setTotalSupply(Number(supply)); // Convert BigInt to number
     } catch (error) {
       console.error('Failed to fetch total supply:', error);
     }
@@ -58,12 +58,18 @@ function App() {
   const connectWallet = async () => {
     try {
       console.log('Attempting to connect wallet...');
-      const accounts = await web3.eth.getAccounts();
-      console.log('Connected accounts:', accounts);
-      setAccount(accounts[0]);
-      fetchTotalSupply(contract); // Fetch total supply after connecting wallet
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('Connected accounts:', accounts);
+        setAccount(accounts[0]);
+        fetchTotalSupply(contract); // Fetch total supply after connecting wallet
+      } else {
+        console.error('MetaMask is not installed.');
+        setFeedback('Please install MetaMask to connect your wallet.');
+      }
     } catch (error) {
       console.error('Failed to connect to wallet:', error);
+      setFeedback('Failed to connect to wallet.');
     }
   };
 
@@ -118,81 +124,32 @@ function App() {
 
   return (
     <div className="Screen">
-      <div
-        className="Container"
-        style={{
-          padding: 24,
-          backgroundColor: '#ADACE6',
-        }}
-      >
+      <div className="Container" style={{ padding: 24, backgroundColor: '#ADACE6' }}>
         <div className="ResponsiveWrapper" style={{ padding: 24 }}>
-          <div
-            className="Container"
-            style={{
-              backgroundColor: '#00b7c3',
-              borderRadius: '24px',
-              flex: 2,
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-          >
-            <p
-              className="TextTitle"
-              style={{
-                textAlign: "center",
-                fontSize: 48,
-                fontWeight: "bold",
-                color: "var(--accent-text2)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+          <div className="Container" style={{ backgroundColor: '#00b7c3', borderRadius: '24px', flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+            <p className="TextTitle" style={{ textAlign: "center", fontSize: 48, fontWeight: "bold", color: "var(--accent-text2)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               Baby Ape Pulse Club
             </p>
             <div className="SpacerSmall" />
-            <p
-              className="TextTitle"
-              style={{
-                textAlign: "center",
-                fontSize: 36,
-                fontWeight: "",
-                color: "var(--secondary-text)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+            <p className="TextTitle" style={{ textAlign: "center", fontSize: 36, fontWeight: "", color: "var(--secondary-text)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               {totalSupply} / {CONFIG.MAX_SUPPLY}
             </p>
             <div className="SpacerLarge" />
-            <p
-              className="TextDescription"
-              style={{
-                textAlign: "center",
-                color: "var(--secondary-text)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+            <p className="TextDescription" style={{ textAlign: "center", color: "var(--secondary-text)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               Mint your exclusive Baby Ape Pulse Club NFT on PulseChain!
             </p>
             <div className="SpacerSmall" />
             {Number(totalSupply) >= CONFIG.MAX_SUPPLY ? (
               <>
                 <div className="SpacerSmall" />
-                <p
-                  className="TextTitle"
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
+                <p className="TextTitle" style={{ textAlign: "center", color: "var(--accent-text)" }}>
                   The sale has ended.
                 </p>
-                <p
-                  className="TextDescription"
-                  style={{ textAlign: "center", color: "var(--accent-text)" }}
-                >
+                <p className="TextDescription" style={{ textAlign: "center", color: "var(--accent-text)" }}>
                   You can still find {CONFIG.NFT_NAME} on {CONFIG.MARKETPLACE}
                 </p>
                 <div className="SpacerSmall" />
-                <button
-                  className="StyledButton"
-                  onClick={() => window.open(CONFIG.MARKETPLACE_LINK, "_blank")}
-                >
+                <button className="StyledButton" onClick={() => window.open(CONFIG.MARKETPLACE_LINK, "_blank")}>
                   {CONFIG.MARKETPLACE}
                 </button>
               </>
@@ -205,81 +162,47 @@ function App() {
                     <button className="StyledButton" onClick={connectWallet}>
                       <img src="/images/connect.png" alt="Connect Wallet" />
                     </button>
-                    {feedback !== "" ? (
+                    {feedback !== "" && (
                       <>
                         <div className="SpacerSmall" />
-                        <p
-                          className="TextDescription"
-                          style={{
-                            textAlign: "center",
-                            color: "var(--accent-text)",
-                          }}
-                        >
+                        <p className="TextDescription" style={{ textAlign: "center", color: "var(--accent-text)" }}>
                           {feedback}
                         </p>
                       </>
-                    ) : null}
+                    )}
                   </div>
                 ) : (
                   <>
-                    <p
-                      className="TextDescription"
-                      style={{
-                        textAlign: "center",
-                        color: "var(--accent-text)",
-                      }}
-                    >
+                    <p className="TextDescription" style={{ textAlign: "center", color: "var(--accent-text)" }}>
                       {feedback}
                     </p>
                     <div className="SpacerMedium" />
                     <div className="Container" style={{ alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                      <button
-                        className="StyledRoundButton"
-                        style={{ lineHeight: 0.4 }}
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          decrementMintAmount();
-                        }}
-                      >
+                      <button className="StyledRoundButton" style={{ lineHeight: 0.4 }} disabled={claimingNft ? 1 : 0} onClick={(e) => {
+                        e.preventDefault();
+                        decrementMintAmount();
+                      }}>
                         -
                       </button>
                       <div className="SpacerMedium" />
-                      <p
-                        className="TextDescription"
-                        style={{
-                          textAlign: "center",
-                          color: "var(--accent-text)",
-                        }}
-                      >
+                      <p className="TextDescription" style={{ textAlign: "center", color: "var(--accent-text)" }}>
                         {mintAmount}
                       </p>
                       <div className="SpacerMedium" />
-                      <button
-                        className="StyledRoundButton"
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          incrementMintAmount();
-                        }}
-                      >
+                      <button className="StyledRoundButton" disabled={claimingNft ? 1 : 0} onClick={(e) => {
+                        e.preventDefault();
+                        incrementMintAmount();
+                      }}>
                         +
                       </button>
                     </div>
                     <div className="SpacerSmall" />
                     <div className="Container" style={{ alignItems: "center", justifyContent: "center", flexDirection: "row" }}>
-                      <button
-                        className="StyledButton"
-                        disabled={claimingNft ? 1 : 0}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          claimNFTs();
-                        }}
-                      >
-                        <img
-                          src={claimingNft ? "/images/busy.png" : "/images/buy.png"}
-                          alt={claimingNft ? "Busy" : "Buy"}
-                        />
+                      <button className="StyledButton" disabled={claimingNft ? 1 : 0} onClick={(e) => {
+                        e.preventDefault();
+                        claimNFTs();
+                      }}>
+                        <img src={claimingNft ? "/images/busy.png" : "/images/buy.png"} alt={claimingNft ? "Busy" : "Buy"} />
                       </button>
                     </div>
                   </>
@@ -296,48 +219,18 @@ function App() {
         <div className="SpacerMedium" />
         <div className="Container" style={{ justifyContent: "center", alignItems: "center", width: "70%" }}>
           <div className="ContentBox">
-            <p
-              className="TextTitle"
-              style={{
-                textAlign: "center",
-                color: "var(--accent-text2)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-                fontSize: "36px",
-              }}
-            >
+            <p className="TextTitle" style={{ textAlign: "center", color: "var(--accent-text2)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)", fontSize: "36px" }}>
               About the Baby Ape Pulse Club
             </p>
-            <p
-              className="TextDescription"
-              style={{
-                textAlign: "center",
-                color: "var(--secondary-text)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+            <p className="TextDescription" style={{ textAlign: "center", color: "var(--secondary-text)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               The Baby Ape Pulse Club (BAPC) is an NFT collection on PulseChain. These NFTs are a free drop for all Pulse Ape Yacht Club (PAYC) holders, with a limit of 20 NFTs per wallet/holder. You only need to cover the gas fees.
             </p>
           </div>
           <div className="ContentBox">
-            <p
-              className="TextTitle"
-              style={{
-                textAlign: "center",
-                color: "var(--accent-text2)",
-                fontSize: "36px",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+            <p className="TextTitle" style={{ textAlign: "center", color: "var(--accent-text2)", fontSize: "36px", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               How to Mint
             </p>
-            <p
-              className="TextDescription"
-              style={{
-                textAlign: "center",
-                color: "var(--secondary-text)",
-                textShadow: "4px 4px 8px rgba(0, 0, 0, .8)",
-              }}
-            >
+            <p className="TextDescription" style={{ textAlign: "center", color: "var(--secondary-text)", textShadow: "4px 4px 8px rgba(0, 0, 0, .8)" }}>
               Connect your wallet, select the number of NFTs you want to mint (up to 20 per wallet), and click the mint button. Ensure you have enough PLS to cover the gas fees. Enjoy your exclusive Baby Ape Pulse Club NFTs!
             </p>
           </div>
--- src/connectors.js
+++ src/connectors.js
@@ -1,10 +1,10 @@
-// src/connectors.js
 import Web3 from 'web3';
 
 const PULSECHAIN_RPC_URL = 'https://rpc.pulsechain.com';
 
 const getWeb3 = async () => {
   console.log('Checking for Ethereum provider...');
+  
   if (window.ethereum) {
     console.log('Ethereum provider detected.');
     const web3 = new Web3(window.ethereum);
